9f2b681650b18a0605961af983b35844ca609897
gerardo montivero <gerardo.montivero@gmail.com>
2022-12-26 07:30:06 -0300

    :tada: Init

---
+++ src/Controller/CartController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Color;
+use App\Entity\Product;
+use App\Form\AddItemToCartFormType;
+use App\Repository\CategoryRepository;
+use App\Repository\ColorRepository;
+use App\Repository\ProductRepository;
+use App\Service\CartStorage;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart", name="app_cart")
+     */
+    public function shoppingCart(CartStorage $cartStorage, ProductRepository $productRepository): Response
+    {
+        $featuredProduct = $productRepository->findFeatured();
+        $addToCartForm = $this->createForm(AddItemToCartFormType::class, null, [
+            'product' => $featuredProduct,
+        ]);
+
+        return $this->render('cart/cart.html.twig', [
+            'cart' => $cartStorage->getOrCreateCart(),
+            'featuredProduct' => $featuredProduct,
+            'addToCartForm' => $addToCartForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/product/{id}", name="app_cart_add_item", methods={"POST"})
+     */
+    public function addItemToCart(Product $product, Request $request, CategoryRepository $categoryRepository, CartStorage $cartStorage)
+    {
+        $addToCartForm = $this->createForm(AddItemToCartFormType::class, null, compact('product'));
+
+        $addToCartForm->handleRequest($request);
+        if ($addToCartForm->isSubmitted() && $addToCartForm->isValid()) {
+            $cart = $cartStorage->getOrCreateCart();
+            $cart->addItem($addToCartForm->getData());
+            $cartStorage->save($cart);
+
+            $this->addFlash('success', 'Item added!');
+
+            return $this->redirectToRoute('app_product', [
+                'id' => $product->getId(),
+            ]);
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+            'categories' => $categoryRepository->findAll(),
+            'addToCartForm' => $addToCartForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/cart/remove/{productId}/{colorId?}", name="app_cart_remove_item", methods={"POST"})
+     */
+    public function removeItemToCart($productId, $colorId, Request $request, CartStorage $cartStorage, ProductRepository $productRepository, ColorRepository $colorRepository)
+    {
+        /** @var Product|null $product */
+        $product = $productRepository->find($productId);
+        /** @var Color|null $color */
+        $color = $colorId ? $colorRepository->find($colorId) : null;
+
+        if (!$product) {
+            $this->createNotFoundException();
+        }
+
+        if (!$this->isCsrfTokenValid('remove_item', $request->request->get('_token'))) {
+            throw new BadRequestHttpException('Invalid CSRF token');
+        }
+
+        $cart = $cartStorage->getOrCreateCart();
+        $cartItem = $cart->findItem($product, $color);
+        if ($cartItem) {
+            $cart->removeItem($cartItem);
+        }
+        $cartStorage->save($cart);
+
+        $this->addFlash('success', 'Item removed!');
+
+        return $this->redirectToRoute('app_cart');
+    }
+
+
+    /**
+     * @Route("/cart/list", name="_app_cart_list")
+     */
+    public function _shoppingCartList(CartStorage $cartStorage): Response
+    {
+        return $this->render('cart/_cartList.html.twig', [
+            'cart' => $cartStorage->getOrCreateCart()
+        ]);
+    }
+}
